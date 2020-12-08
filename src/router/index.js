de103966f24eed2d4d970f1f97df152386d99af8
hjj14796353396 <1808136161@qq.com>
2020-12-08 21:51:34 +0800

    axios学习

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,7 +15,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/2-2',
+    name: 'axios请求方法',
+    component: () => import('../views/2-2.vue')
+  },
+  {
+    path: '/2-3',
+    name: 'axios并发请求',
+    component: () => import('../views/2-3.vue')
+  },
+  {
+    path: '/contactList',
+    name: '联系人列表',
+    component: () => import('../views/ContactList.vue')
   }
 ]
 
---
+++ src/views/2-3.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  //并发请求：同时进行多个请求，并统一处理返回值
+  name: "axios2-3",
+  created() {
+    //axios.all() axios.spread()
+    // axios.get("/data.json").then((res) => {
+    //   console.log(res);
+    // });
+    axios.all(
+      [
+      axios.get('/data.json'),
+      axios.get('/city.json')
+      ]
+    ).then(
+      axios.spread((dataRes,cityRes)=>{
+        console.log(dataRes,cityRes)
+      })
+    )
+  },
+};
+</script>
